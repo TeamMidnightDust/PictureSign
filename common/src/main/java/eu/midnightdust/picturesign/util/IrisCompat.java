479542175ba14406ca605bd85a28a15a1047aa36
Martin Prokoph <motschen@midnightdust.eu>
2024-06-20 23:23:08 +0200

    Re-add Iris compatibility

--- common/src/main/java/eu/midnightdust/picturesign/util/IrisCompat.java
+++ common/src/main/java/eu/midnightdust/picturesign/util/IrisCompat.java
@@ -1,9 +1,9 @@
 package eu.midnightdust.picturesign.util;
 
-//import net.irisshaders.iris.api.v0.IrisApi;
+import net.irisshaders.iris.api.v0.IrisApi;
 
 public class IrisCompat {
     public static boolean isShaderPackInUse() {
-        return false;//IrisApi.getInstance().isShaderPackInUse();
+        return IrisApi.getInstance().isShaderPackInUse();
     }
 }
