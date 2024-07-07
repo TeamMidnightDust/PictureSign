fcdde95971a33f7e05a44fd58570734afc9b1cab
Martin Prokoph <motschen@midnightdust.eu>
2024-07-07 19:09:36 +0200

    Remove leftover field in Neoforge

--- neoforge/src/main/java/eu/midnightdust/picturesign/neoforge/PictureSignClientNeoForge.java
+++ neoforge/src/main/java/eu/midnightdust/picturesign/neoforge/PictureSignClientNeoForge.java
@@ -1,20 +1,14 @@
 package eu.midnightdust.picturesign.neoforge;
 
 import eu.midnightdust.picturesign.PictureSignClient;
-import net.minecraft.resource.ResourcePackProfile;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
-import org.apache.commons.compress.utils.Lists;
-
-import java.util.List;
 
 import static eu.midnightdust.picturesign.PictureSignClient.MOD_ID;
 
 @SuppressWarnings("all")
 @Mod(value = MOD_ID, dist = Dist.CLIENT)
 public class PictureSignClientNeoForge {
-    public static List<ResourcePackProfile> defaultEnabledPacks = Lists.newArrayList();
-
     public PictureSignClientNeoForge() {
         PictureSignClient.init();
     }
